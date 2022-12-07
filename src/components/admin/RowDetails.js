4e308cea76b69ddff7cb7c46802f81aa52461954
Ridham Modh <ridhs193@gmail.com>
2022-12-07 18:04:58 -0500

    feat-admin : Added admin component

---
+++ src/components/admin/RowDetails.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function RowDetails({ Email, Lastname, Firstname, Id, IsAdmin, OnDelete }) {
+  return (
+    <tr>
+      <th>{Email}</th>
+      <td>{Lastname}</td>
+      <td>{Firstname}</td>
+      <td>{IsAdmin}</td>
+      <td className="gap__actions">
+        {/* <span className="badge bg-info">
+          <Link to={`admin/${Id}`} className="text-white">
+            <i className="fas fa-edit"></i>
+          </Link>
+        </span> */}
+        <button className="btn btn-primary" type="submit">
+          <Link to={`admin/${Id}`} className="text-white">
+            Edit
+          </Link>
+        </button>
+
+        {/* <span className="badge bg-danger" onClick={() => OnDelete(Id)}>
+          <i className="fas fa-trash-alt"></i>
+        </span> */}
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={() => OnDelete(Id)}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+export default RowDetails;
